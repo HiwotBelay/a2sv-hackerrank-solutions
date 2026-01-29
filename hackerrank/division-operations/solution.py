62e27f194b96ad610a4612306cacf955e25cb025
Hiwot Belay <hiwotbelay060@gmail.com>
2026-01-29 23:46:20 +0300

    Add division operations solution

---
+++ hackerrank/division-operations/solution.py
@@ -0,0 +1,13 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    # Integer division (floor division)
+    integer_division = a // b
+    
+    # Float division (normal division)
+    float_division = a / b
+    
+    # Print the results
+    print(integer_division)
+    print(float_division)
