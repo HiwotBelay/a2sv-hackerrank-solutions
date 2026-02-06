0fbad63f25f76d7bb73ec16eeb936f78c99382a8
Hiwot Belay <hiwotbelay060@gmail.com>
2026-02-06 21:43:50 +0300

    Add power mod power solution

---
+++ hackerrank/power-mod-power/solution.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    m = int(input())
+    
+    # Print a^b
+    print(pow(a, b))
+    
+    # Print a^b mod m
+    print(pow(a, b, m))
